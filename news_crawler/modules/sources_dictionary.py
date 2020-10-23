ffcaf8bae4153e70665b3409e5aff0ea7691c101
Andy Clarke <andrewjhclarke@gmail.com>
2020-10-23 18:27:59 +0100

    Relaunch

--- news_crawler/modules/sources_dictionary.py
+++ news_crawler/modules/sources_dictionary.py
@@ -132,28 +132,28 @@ sources_dictionary = {
 		"article_url_prefix": "",
 		"fashionb2b": True
 	},
-	# "Dansk Mode & Textil": {
-	# 	"country": "Denmark",
-	# 	"language": "Danish",
-	# 	"landing_urls": dansk_url_list_creator(),
-	# 	"landing_characteristics": '//div[contains(@class, "column-block")]//a/@href',
-	# 	"article_characteristics": "//p",
-	# 	"headline_characteristics": "//h1",
-	# 	"published_date_characteristics": "//span[contains(@class, 'article__detail')]",
-	# 	"article_url_prefix": "https://www.dmogt.dk",
-	# 	"fashionb2b": True
-	# },
-	# "Modaes": {
-	# 	"country": "Spain",
-	# 	"language": "Spanish",
-	# 	"landing_urls": modaes_url_list_creator(),
-	# 	"landing_characteristics": '//div[contains(@class, "title")]//a/@href',
-	# 	"article_characteristics": "//p",
-	# 	"headline_characteristics": "//h1[contains(@class, 'title')]",
-	# 	"published_date_characteristics": "//div[contains(@class, 'news_block_title')]",
-	# 	"article_url_prefix": "https://www.modaes.es/",
-	# 	"fashionb2b": True
-	# },
+	"Dansk Mode & Textil": {
+		"country": "Denmark",
+		"language": "Danish",
+		"landing_urls": dansk_url_list_creator(),
+		"landing_characteristics": '//div[contains(@class, "column-block")]//a/@href',
+		"article_characteristics": "//p",
+		"headline_characteristics": "//h1",
+		"published_date_characteristics": "//span[contains(@class, 'article__detail')]",
+		"article_url_prefix": "https://www.dmogt.dk",
+		"fashionb2b": True
+	},
+	"Modaes": {
+		"country": "Spain",
+		"language": "Spanish",
+		"landing_urls": modaes_url_list_creator(),
+		"landing_characteristics": '//div[contains(@class, "title")]//a/@href',
+		"article_characteristics": "//p",
+		"headline_characteristics": "//h1[contains(@class, 'title')]",
+		"published_date_characteristics": "//div[contains(@class, 'news_block_title')]",
+		"article_url_prefix": "https://www.modaes.es/",
+		"fashionb2b": True
+	},
 	# "Fashion Magazine Italy - Business": {
 	# 	"country": "Italy",
 	# 	"language": "Italian",
