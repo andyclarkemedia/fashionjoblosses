1f4795b603290dfb73ebeac1a3cb403e3681dd46
Andy Clarke <andrewjhclarke@gmail.com>
2020-10-23 13:32:57 +0100

    relaunch

--- news_crawler/modules/sources_dictionary.py
+++ news_crawler/modules/sources_dictionary.py
@@ -22,50 +22,50 @@ from ..modules.url_helpers import reuters_url_list_creator, wwd_url_list_creator
 sources_dictionary = {
 
 
-	"Drapers - Home": {
-		"country": "United Kingdom",
-		"language": "English",
-		"landing_urls": ["https://www.drapersonline.com/"],
-		"landing_characteristics": '//div[contains(@id, "main-content")]//a/@href',
-		"article_characteristics": "//p",
-		"headline_characteristics": "//h1",
-		"published_date_characteristics": "//span[contains(@class, 'tie-date')]",
-		"article_url_prefix": "",
-		"fashionb2b": True
-	},
-	"Drapers - Redundancies": {
-		"country": "United Kingdom",
-		"language": "English",
-		"landing_urls": ["https://www.drapersonline.com/tag/redundancies", "https://www.drapersonline.com/tag/redundancies/page/2", "https://www.drapersonline.com/tag/redundancies/page/3", "https://www.drapersonline.com/tag/redundancies/page/4", "https://www.drapersonline.com/tag/redundancies/page/5", "https://www.drapersonline.com/tag/redundancies/page/6"],
-		"landing_characteristics": '//div[contains(@id, "main-content")]//a/@href',
-		"article_characteristics": "//p",
-		"headline_characteristics": "//h1",
-		"published_date_characteristics": "//span[contains(@class, 'tie-date')]",
-		"article_url_prefix": "",
-		"fashionb2b": True
-	},
-	"Retail Detail - Home": {
-		"country": "Europe",
-		"language": "English",
-		"landing_urls": ["https://www.retaildetail.eu/en"],
-		"landing_characteristics": '//a[contains(@href, "https")]/@href',
-		"article_characteristics": "//p",
-		"headline_characteristics": "//h1",
-		"published_date_characteristics": "//div[contains(@class, 'date')]",
-		"article_url_prefix": "",
-		"fashionb2b": True
-	},
-	"Retail Detail - Fashion": {
-		"country": "Europe",
-		"language": "English",
-		"landing_urls": ["https://www.retaildetail.eu/en/fashion", "https://www.retaildetail.eu/en/fashion?page=1", "https://www.retaildetail.eu/en/fashion?page=2", "https://www.retaildetail.eu/en/fashion?page=3", "https://www.retaildetail.eu/en/fashion?page=4", "https://www.retaildetail.eu/en/fashion?page=5", "https://www.retaildetail.eu/en/fashion?page=6", "https://www.retaildetail.eu/en/fashion?page=7", "https://www.retaildetail.eu/en/fashion?page=8", "https://www.retaildetail.eu/en/fashion?page=9", "https://www.retaildetail.eu/en/fashion?page=10", "https://www.retaildetail.eu/en/fashion?page=11"],
-		"landing_characteristics": '//a[contains(@href, "https")]/@href',
-		"article_characteristics": "//p",
-		"headline_characteristics": "//h1",
-		"published_date_characteristics": "//div[contains(@class, 'date')]",
-		"article_url_prefix": "",
-		"fashionb2b": True
-	},
+	# "Drapers - Home": {
+	# 	"country": "United Kingdom",
+	# 	"language": "English",
+	# 	"landing_urls": ["https://www.drapersonline.com/"],
+	# 	"landing_characteristics": '//div[contains(@id, "main-content")]//a/@href',
+	# 	"article_characteristics": "//p",
+	# 	"headline_characteristics": "//h1",
+	# 	"published_date_characteristics": "//span[contains(@class, 'tie-date')]",
+	# 	"article_url_prefix": "",
+	# 	"fashionb2b": True
+	# },
+	# "Drapers - Redundancies": {
+	# 	"country": "United Kingdom",
+	# 	"language": "English",
+	# 	"landing_urls": ["https://www.drapersonline.com/tag/redundancies", "https://www.drapersonline.com/tag/redundancies/page/2", "https://www.drapersonline.com/tag/redundancies/page/3", "https://www.drapersonline.com/tag/redundancies/page/4", "https://www.drapersonline.com/tag/redundancies/page/5", "https://www.drapersonline.com/tag/redundancies/page/6"],
+	# 	"landing_characteristics": '//div[contains(@id, "main-content")]//a/@href',
+	# 	"article_characteristics": "//p",
+	# 	"headline_characteristics": "//h1",
+	# 	"published_date_characteristics": "//span[contains(@class, 'tie-date')]",
+	# 	"article_url_prefix": "",
+	# 	"fashionb2b": True
+	# },
+	# "Retail Detail - Home": {
+	# 	"country": "Europe",
+	# 	"language": "English",
+	# 	"landing_urls": ["https://www.retaildetail.eu/en"],
+	# 	"landing_characteristics": '//a[contains(@href, "https")]/@href',
+	# 	"article_characteristics": "//p",
+	# 	"headline_characteristics": "//h1",
+	# 	"published_date_characteristics": "//div[contains(@class, 'date')]",
+	# 	"article_url_prefix": "",
+	# 	"fashionb2b": True
+	# },
+	# "Retail Detail - Fashion": {
+	# 	"country": "Europe",
+	# 	"language": "English",
+	# 	"landing_urls": ["https://www.retaildetail.eu/en/fashion", "https://www.retaildetail.eu/en/fashion?page=1", "https://www.retaildetail.eu/en/fashion?page=2", "https://www.retaildetail.eu/en/fashion?page=3", "https://www.retaildetail.eu/en/fashion?page=4", "https://www.retaildetail.eu/en/fashion?page=5", "https://www.retaildetail.eu/en/fashion?page=6", "https://www.retaildetail.eu/en/fashion?page=7", "https://www.retaildetail.eu/en/fashion?page=8", "https://www.retaildetail.eu/en/fashion?page=9", "https://www.retaildetail.eu/en/fashion?page=10", "https://www.retaildetail.eu/en/fashion?page=11"],
+	# 	"landing_characteristics": '//a[contains(@href, "https")]/@href',
+	# 	"article_characteristics": "//p",
+	# 	"headline_characteristics": "//h1",
+	# 	"published_date_characteristics": "//div[contains(@class, 'date')]",
+	# 	"article_url_prefix": "",
+	# 	"fashionb2b": True
+	# },
 	"Reuters - Business News": {
 		"country": "Global",
 		"language": "English",
