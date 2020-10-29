dbad52fd6820a777ff4485a833f011969c53a4bf
Andy Clarke <andrewjhclarke@gmail.com>
2020-10-29 15:19:34 +0000

    Secret Key Hidden

--- fashionjoblosses/settings_local.py
+++ fashionjoblosses/settings_local.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^mn76f6f5y4(cp#0(&_)pir!5=7i$&ybcjv(dj$=wi3)215dqy'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
